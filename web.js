75dedf42c586ad843419803c01f7212bf0887825
Dean Nicholls <dean.redblufftech@gmail.com>
2013-07-13 08:14:17 +0000

    First go at adding index write

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
+// 13/7/13 DHN Applied modifications to web.js as required to write the 
+//             data from the index.html file
+//
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  fs.readFileSync('index.html', function (err, data) {
+    if (err) throw (err);
+    response.send(data.toString('utf-8'));
+  });
 });
 
 var port = process.env.PORT || 5000;
